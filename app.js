3f7a9700347321b4810f855581d1b6d6783591c7
Akshay <akshayshriram999@gmail.com>
2025-12-25 20:47:52 +0530

    add Arcjet middleware for Rate limit and prevent Bot Attacks...

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabse from "./database/mongoDB.js";
 import errorMiddleware from "./middleware/error.middleware.js";
+import arcjetMiddleware from "./middleware/arcjet.middleware.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
