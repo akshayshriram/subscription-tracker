2e7ac9ba4f3a78f0c37f2f89a9a71132c8516d3a
Akshay <akshayshriram999@gmail.com>
2025-12-25 19:28:11 +0530

    Implement user routes and authorization middleware with user fetching logic

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
+import { getUsers, getUserById } from "../controllers/user.controller.js";
+import { authorize } from "../middleware/auth.middleware.js";
+const userRouter = Router();
 
-const userRouter = Router()
+userRouter.get("/", getUsers);
+userRouter.get("/:id", authorize, getUserById);
+userRouter.post("/", (req, res) =>
+  res.send({ status: 201, message: "Create new user" })
+);
+userRouter.put("/:id", (req, res) =>
+  res.send({ status: 200, message: "Update the user" })
+);
+userRouter.delete("/:id", (req, res) => res.send({ message: "Deleted users" }));
 
-userRouter.get("/", (req,res) => res.send({status: 200, message:"Get all users"}))
-userRouter.get("/:id", (req,res) => res.send({status: 200, message:"Get user details"}))
-userRouter.post("/", (req,res) => res.send({status: 201, message:"Create new user"}))
-userRouter.put("/:id", (req,res) => res.send({status: 200, message:"Update the user"}))
-userRouter.delete("/:id", (req,res) => res.send({message:"Deleted users"}))
-
-export default userRouter
+export default userRouter;
